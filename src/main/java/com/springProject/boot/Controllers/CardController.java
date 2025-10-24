0cfb51bad628eaa7de28d874285175adce62fa17
abdkad123 <akaddoura123@gmail.com>
2025-10-24 04:53:54 +0300

    fixed url typo

--- src/main/java/com/springProject/boot/Controllers/CardController.java
+++ src/main/java/com/springProject/boot/Controllers/CardController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/card-controller")
+@RequestMapping("/card")
 public class CardController {
     @Autowired
     CardService cardService;
