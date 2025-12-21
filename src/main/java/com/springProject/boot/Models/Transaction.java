df25bdbcd3cce8a9545dbe6d9412b9f807288902
abdkad123 <akaddoura123@gmail.com>
2025-12-21 23:59:43 +0200

    Test: testing pushing to github from intellij

---
+++ src/main/java/com/springProject/boot/Models/Transaction.java
@@ -0,0 +1,23 @@
+package com.springProject.boot.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transaction")
+@Getter
+@Setter
+@Audited
+public class Transaction {
+    @Id
+    @Column(name = "transaction_id")
+    private UUID id;
+
+}
