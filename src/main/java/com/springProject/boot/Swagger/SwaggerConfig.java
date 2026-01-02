420157aa2ad81fee8b20e83150832d11452df6ff
abdkad123 <akaddoura123@gmail.com>
2026-01-02 18:01:25 +0200

    feat: configured swagger

--- src/main/java/com/springProject/boot/Swagger/SwaggerConfig.java
+++ src/main/java/com/springProject/boot/Swagger/SwaggerConfig.java
@@ -1,4 +1,21 @@
 package com.springProject.boot.Swagger;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info().title("cms API's").description("API documentation").version("1.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder().group("public").packagesToScan("com.springProject.boot").build();
+    }
 }
